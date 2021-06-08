99d27ff6dbee585182c90bc4291bf9e6ab535e93
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-06-08 08:12:10 +0700

    Add files via upload

---
+++ translation.py
@@ -0,0 +1,16 @@
+class Translation(object):
+    START_TEXT = """Hai!{} ngab
+mau cari API ID & API HASH Telegram sini ngab gue bantu yang pastinya simple.
+Silahkan Masukkan Nomor Telepon Telegram Ngab bukan no bh Dengan Format +62(nohp).
+"""
+    AFTER_RECVD_CODE_TEXT = """No HP Diterima Ngab!
+Sabi kan ngab kirim kode Telegram nya kesini!
+
+Kode ini hanya digunakan untuk tujuan mendapatkan ID APP dari my.telegram.org
+jika Anda tidak mempercayai dev bot ini, Ngambil Manual aja Goblokk 😁🙏
+"""
+    BEFORE_SUCC_LOGIN = "Kode Diterima. Scarpping Web Page..."
+    ERRED_PAGE = "Hadeh Error Ngab. Gagal Mendapatkan id aplikasi. \n\n Kontak saya : @kenkanasw"
+    CANCELLED_MESG = "Bye! Silahkan /start kembali ngab untuk mengulang"
+    IN_VALID_CODE_PVDED = "Masukan Kode Telegram yang Bener Goblok 😁🙏"
+    IN_VALID_PHNO_PVDED = "Masukan NO HP yang Bener Goblok 😁🙏 Pake Format +(kode Negara) Contoh +62(nohp)"
