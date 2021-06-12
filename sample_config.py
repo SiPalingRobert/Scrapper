fc2446d44914ada6b07960c889e68b9499a94f75
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-06-12 08:27:48 +0700

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -12,9 +12,9 @@ class Config:
     CHUNK_SIZE = 10280
     # MyTelegram.org
     # configurtion required while creating new application
-    APP_TITLE = os.environ.get("APP_TITLE")
-    APP_SHORT_NAME = os.environ.get("APP_SHORT_NAME")
-    APP_URL = os.environ.get("APP_URL")
+    APP_TITLE = os.environ.get("APP_TITLE", "Bukanahlirobot")
+    APP_SHORT_NAME = os.environ.get("APP_SHORT_NAME", "Bukanahlirobot")
+    APP_URL = os.environ.get("APP_URL", "https://telegram.dog/Bukanahlirobot")
     # these platform informations were obtained
     # on 27.01.2020 21:15:50 IST
     APP_PLATFORM = [
@@ -32,7 +32,7 @@ class Config:
     # https://github.com/kenkannih/scraper-ken
     APP_DESCRIPTION = os.environ.get(
         "APP_DESCRIPTION",
-        "created using https:https://telegram.dog/UseTGxBot"
+        "created using https:https://telegram.dog/Bukanahlirobot"
     )
     #
     FOOTER_TEXT = os.environ.get("FTEXT", "Managed With ☕️ By @kenkanasw")
